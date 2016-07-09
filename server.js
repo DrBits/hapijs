8631ffa9327f5fe82d99e2fcbe13906012f108e0
Aleksei Levanov <Aleksei Levanov>
2016-07-09 06:28:08 -0800

    Init server; create route to path '/'

---
+++ server.js
@@ -0,0 +1,22 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+
+server.connection({ port: 3000 });
+
+server.ext('onRequest', function(request, reply) {
+	console.log('Request received: ' + request.path);
+	reply.continue();
+});
+
+server.route({
+	path: '/',
+	method: 'GET',
+	handler: {
+		file: 'templates/index.html'
+	}
+});
+
+server.start(function() {
+	console.log('Listening on ' + server.info.uri);
+});
